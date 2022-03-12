28dc1d6a76bddfd68c7bafc32be80ff38f846e33
Harsh Chauhan <79708894+harshchauhan123@users.noreply.github.com>
2022-03-13 01:32:17 +0530

    Update attendance.py

--- Attendance_system_using_Face_Recognition/attendance.py
+++ Attendance_system_using_Face_Recognition/attendance.py
@@ -4,7 +4,7 @@ import face_recognition
 import os
 from datetime import datetime
 
-
+ct = 0
 path = 'images'
 images = []
 personNames = []
@@ -26,7 +26,7 @@ def faceEncodings(images):
     return encodeList
 
 
-def attendance(name):
+def attendance(name, ct):
     with open('Attendance.csv', 'r+') as f:
         myDataList = f.readlines()
         nameList = []
@@ -37,7 +37,7 @@ def attendance(name):
             time_now = datetime.now()
             tStr = time_now.strftime('%H:%M:%S')
             dStr = time_now.strftime('%d/%m/%Y')
-            f.writelines(f'\n{name},{tStr},{dStr}')
+            f.write(f'\n   {name},{tStr},{dStr}')
 
 
 encodeListKnown = faceEncodings(images)
@@ -56,22 +56,33 @@ while True:
     for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        # print(faceDis)
+        #print(faceDis)
         matchIndex = np.argmin(faceDis)
 
         if matches[matchIndex]:
             name = personNames[matchIndex].upper()
             print(name)
+            ct = ct+1
+            print(ct)
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            attendance(name)
+            attendance(name, ct)
 
     cv2.imshow('Webcam', frame)
-    if cv2.waitKey(1) == 13:
+    if cv2.waitKey(1000) == 13:
         break
+
+    def attendance(name, ct):
+        with open('Attendance.csv', 'r+') as f:
+            f.writelines(f'\n{int(ct/60)}')
+
+
+
+
 
 cap.release()
 cv2.destroyAllWindows()
+
